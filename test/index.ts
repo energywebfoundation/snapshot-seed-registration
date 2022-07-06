8cee72a06743de384ccf7aa26db656e55d0fce4b
John Henderson <john.henderson@energyweb.org>
2022-07-06 17:46:52 +0200

    test: clarify that updates are possibly

--- test/index.ts
+++ test/index.ts
@@ -2,18 +2,32 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("SnapshotSeedRegistration", function () {
-  it("Should allow registration of snapshot block", async function () {
+  it("Should allow registration and update of snapshot block", async function () {
     const SnapshotSeedRegistration = await ethers.getContractFactory(
       "SnapshotSeedRegistration"
     );
     const registration = await SnapshotSeedRegistration.deploy();
     await registration.deployed();
 
-    const setGreetingTx = await registration.registerSnapshotSeed(1, 18000000);
+    const snapshotNumber = 1;
+    const snapshotSeedBlock1 = 10000000;
+    const registration1Tx = await registration.registerSnapshotSeed(
+      snapshotNumber,
+      snapshotSeedBlock1
+    );
+    await registration1Tx.wait();
+    expect(await registration.getSnapshotSeed(snapshotNumber)).to.equal(
+      snapshotSeedBlock1
+    );
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await registration.getSnapshotSeed(1)).to.equal(18000000);
+    const snapshotSeedBlock2 = 20000000;
+    const registration2Tx = await registration.registerSnapshotSeed(
+      snapshotNumber,
+      snapshotSeedBlock2
+    );
+    await registration2Tx.wait();
+    expect(await registration.getSnapshotSeed(snapshotNumber)).to.equal(
+      snapshotSeedBlock2
+    );
   });
 });
