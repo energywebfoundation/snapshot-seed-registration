5c1220eff024e2324bb7e0e7de8e06615ace2204
John Henderson <john.henderson@energyweb.org>
2022-07-06 17:34:21 +0200

    feat: add SnapshotSeed getter

--- contracts/SnapshotSeedRegistration.sol
+++ contracts/SnapshotSeedRegistration.sol
@@ -5,12 +5,20 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SnapshotSeedRegistration is Ownable {
-  mapping(uint => uint) public snapshotSeedBlocks; 
+  mapping(uint => uint) private snapshotSeedBlocks; 
   
   /*
    * This allows the owner to register which block they will be using for a snapshot seed
+   * TODO: emit event
    */
   function registerSnapshotSeed(uint snapshotNumber, uint seedBlockNumber) public onlyOwner {
     snapshotSeedBlocks[snapshotNumber] = seedBlockNumber;
+  }
+
+  /*
+   * Get the snapshot seed block associated with a snapshot number 
+   */
+  function getSnapshotSeed(uint snapshotNumber) view public returns(uint) {
+    return snapshotSeedBlocks[snapshotNumber];
   }
 }
