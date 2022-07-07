9b829dd379aed7ade1a278430c36a221868aebf4
John Henderson <john.henderson@energyweb.org>
2022-07-07 10:35:22 +0200

    test: only owners can call reg&update

--- test/index.ts
+++ test/index.ts
@@ -31,6 +31,49 @@ describe("SnapshotSeedRegistration", function () {
     );
   });
 
+  it("Should only allow owner to register", async function () {
+    // eslint-disable-next-line no-unused-vars
+    const [owner, addr1] = await ethers.getSigners();
+    const SnapshotSeedRegistration = await ethers.getContractFactory(
+      "SnapshotSeedRegistration"
+    );
+    const registration = await SnapshotSeedRegistration.deploy();
+    await registration.deployed();
+
+    const snapshotNumber = 1;
+    const snapshotSeedBlock = 10000000;
+    await expect(
+      registration
+        .connect(addr1)
+        .registerSnapshotSeed(snapshotNumber, snapshotSeedBlock)
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+
+  it("Should only allow owner to update", async function () {
+    const [owner, addr1] = await ethers.getSigners();
+    const SnapshotSeedRegistration = await ethers.getContractFactory(
+      "SnapshotSeedRegistration"
+    );
+    const registration = await SnapshotSeedRegistration.deploy();
+    await registration.deployed();
+
+    const snapshotNumber = 1;
+    const snapshotSeedBlock = 10000000;
+    const registration1Tx = await registration
+      .connect(owner)
+      .registerSnapshotSeed(snapshotNumber, snapshotSeedBlock);
+    await registration1Tx.wait();
+    expect(await registration.getSnapshotSeed(snapshotNumber)).to.equal(
+      snapshotSeedBlock
+    );
+
+    await expect(
+      registration
+        .connect(addr1)
+        .updateSnapshotSeed(snapshotNumber, snapshotSeedBlock)
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+
   it("Should not allow update to the same seed block", async function () {
     const SnapshotSeedRegistration = await ethers.getContractFactory(
       "SnapshotSeedRegistration"
